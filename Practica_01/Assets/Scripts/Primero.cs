456847e3281a36c9a06fb389db050ef227c17916
yaigh <yaizagh2001@gmail.com>
2021-09-17 18:52:59 +0200

    Ejercicio 2-A
    Numero vaya creciendo

--- Practica_01/Assets/Scripts/Primero.cs
+++ Practica_01/Assets/Scripts/Primero.cs
@@ -4,30 +4,24 @@ using UnityEngine;
 
 public class Primero : MonoBehaviour
 {
-    float mensaje1 = 10f;
-    float mensaje2 = 8f;
-    float mensaje3;
 
-    string men1 = "Hoy lo logro";
-    string men2 = " o puede que no";
-    string men3;
+    int ejem1 = 0;
+    
 
-    int ejem1 = 5;
-    int ejem2 = 5;
-    int ejem3;
-
-    public int var1 = 3;
-    public int var2 = 6;
-    public int var3;
+   
 
     // Start is called before the first frame update
     void Start()
     {
-       
-        //Ambito publico
+        //Numero vaya creciendo
+        while (ejem1 < 10) 
+        {
+            ejem1 ++ ;
+            print(ejem1);
 
-        int var3 = var1 * var2;
-        print(var3);
+        }
+
+
 
     }
 
