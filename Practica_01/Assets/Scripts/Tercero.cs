6083d5fc1c4fcee205458890b136c2317814efc4
yaigh <yaizagh2001@gmail.com>
2021-09-17 20:17:58 +0200

    Prueba 2

--- Practica_01/Assets/Scripts/Tercero.cs
+++ Practica_01/Assets/Scripts/Tercero.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Segundo : MonoBehaviour
+public class Tercero : MonoBehaviour
 {
-   
-
     // Start is called before the first frame update
     void Start()
     {
